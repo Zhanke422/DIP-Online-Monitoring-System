a215d5b1bb88f63f90a3be72354b412f35e72f0c
imJenny614 <55578133+imJenny614@users.noreply.github.com>
2021-03-06 17:22:22 +0800

    Update api.py
    add a function to process video file and change the critical frame number to 60 (about 2s) for presence detection

--- api.py
+++ api.py
@@ -33,7 +33,7 @@ class Student:
         '''
         # set a flag with original value to be True
         presence_flag = True
-        trigger_value = 5
+        trigger_value = 60
         while presence_flag:
             if not self.authentication(image_file):
                 self.missing_frame += 1
@@ -71,3 +71,68 @@ class Student:
         cv2.destroyAllWindows()
         return image_file
 
+    def authentication_return_video(self, video_file):
+        '''
+        A temporary function for reading in a video file and output a file with rectangles around face detected.
+        :param video_file:
+        :return: output video file for demo
+        '''
+        # Open the input movie file
+        input_movie = cv2.VideoCapture(video_file)
+        length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # Create an output movie file (make sure resolution/frame rate matches input video!)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        output_movie = cv2.VideoWriter('student_output.avi', fourcc, 29.97, (1280, 720))
+
+        frame_number = 0
+
+        while True:
+            # Grab a single frame of video
+            ret, frame = input_movie.read()
+            frame_number += 1
+
+            # Quit when the input video file ends
+            if not ret:
+                break
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_frame = frame[:, :, ::-1]
+
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_frame)
+            face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                match = face_recognition.compare_faces(self.face_encoding, face_encoding, tolerance=0.50)
+
+                name = None
+                if match[0]:
+                    name = "Ni Anqi"
+
+                face_names.append(name)
+
+            # Label the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                if not name:
+                    continue
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+            # Write the resulting image to the output video file
+
+            output_movie.write(frame)
+            print("Writing frame {} / {}".format(frame_number, length))
+
+        # All done!
+        input_movie.release()
+        cv2.destroyAllWindows()
+        return output_movie
